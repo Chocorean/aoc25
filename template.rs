db4f42ea046f8bb6f2b933a871326b0f85a15364
Chocorean <chocorean@pm.me>
2025-12-03 09:45:04 -0500

    add skeleton

--- template.rs
+++ template.rs
@@ -1,5 +1,15 @@
 fn main() {
     let input = include_str!("../input.txt");
+    println!("{}", task1(&input));
+    println!("{}", task2(&input));
+}
+
+pub fn task1(input: &str) -> u64 {
+    0
+}
+
+pub fn task2(input: &str) -> u64 {
+    0
 }
 
 #[cfg(test)]
@@ -7,7 +17,14 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_example() {
+    fn test_task1() {
         let input = include_str!("../example.txt");
+        assert_eq!(task1(&input), 0);
+    }
+
+    #[test]
+    fn test_task2() {
+        let input = include_str!("../example.txt");
+        assert_eq!(task2(&input), 0);
     }
 }
